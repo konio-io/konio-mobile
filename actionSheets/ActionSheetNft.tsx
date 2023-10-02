3898feac86f1a0215d4679967a84cb5e03878929
Adriano Foschi <adri.foschi@gmail.com>
2023-10-02 23:06:01 +0200

    fix

--- actionSheets/ActionSheetNft.tsx
+++ actionSheets/ActionSheetNft.tsx
@@ -1,8 +1,8 @@
 import { SheetProps } from "react-native-actions-sheet";
 import { useI18n } from "../hooks";
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
-import { AssetsNavigationProp } from "../types/navigation";
+import { HoldingsNavigationProp } from "../types/navigation";
 import * as Clipboard from 'expo-clipboard';
 import Toast from "react-native-toast-message";
 import { NftStore } from "../stores";
@@ -18,11 +18,13 @@ export default (props: SheetProps<{
     }
 
     const i18n = useI18n();
-    const navigation = useNavigation<AssetsNavigationProp>();
+    const navigation = useNavigation<HoldingsNavigationProp>();
     const nft = NftStore.state.nested(nftId).get();
     
     const _delete = () => {
-        navigation.navigate('Assets');
+        navigation.navigate('Holdings', {
+            screen: 'Assets'
+        });
         setTimeout(() => {
             NftStore.actions.deleteNft(nftId);
         }, 1000)
@@ -62,6 +64,16 @@ export default (props: SheetProps<{
             icon: <AntDesign name="delete"/>,
             onPress: async () => _delete()
         },
+        {
+            title: i18n.t('send'),
+            icon: <Feather name="arrow-up-right" />,
+            onPress: () => navigation.navigate('Withdraw', { nftId: nft.id })
+        },
+        {
+            title: i18n.t('receive'),
+            icon: <Feather name="arrow-down-right" />,
+            onPress: () => navigation.navigate('Deposit')
+        }
     ];
 
     return (
